34a6def58d938756efdce329ffd20d8d802be1ab
Marcelo L Lubasinski <marcelo.gestao.digital@gmail.com>
2018-10-30 20:09:54 -0300

    Corrigido erros na geração dos arquivos

--- Layout básico/index.php
+++ Layout básico/index.php
@@ -9,8 +9,11 @@
 	</div>
 	<div class="menu">Área de menu</div>
 	<div class="row">
-		<div role="main" class="col-md-12">
+		<div role="main" class="col-md-8">
 			<div class="content">Área de conteudo</div>
 		</div>
+		<aside role="complementary" class="col-md-4">
+			<div class="sidebar">Sidebar</div>
+		</aside>
 	</div>
 <?php include_once('footer.php');?>
