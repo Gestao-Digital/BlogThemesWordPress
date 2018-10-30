a21200446107fabd79dbb623124f23f96f5dac16
Marcelo l Lubasinski <marcelo.gestao.digital@gmail.com>
2018-10-30 19:35:44 -0300

    Inclusão dos arquivos da postagem Layout básico

---
+++ Layout básico/index.php
@@ -0,0 +1,16 @@
+<?php include_once('header.php');?>
+<body>
+    <div class="container">
+    <div class="header">
+		<br>
+		<h1><?php bloginfo('name'); ?></h1>
+		<br>
+		<h2><?php bloginfo('description'); ?></h2>
+	</div>
+	<div class="menu">Área de menu</div>
+	<div class="row">
+		<div role="main" class="col-md-12">
+			<div class="content">Área de conteudo</div>
+		</div>
+	</div>
+<?php include_once('footer.php');?>
