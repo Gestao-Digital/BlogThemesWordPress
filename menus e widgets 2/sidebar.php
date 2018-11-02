a7c4cde3198b6f1bf07cb769dced6d7d94377b19
Marcelo L Lubasinski <marcelo.gestao.digital@gmail.com>
2018-11-02 13:46:34 -0300

    Inclusão dos arquivos da postagem menus e widgets 2

---
+++ menus e widgets 2/sidebar.php
@@ -0,0 +1,3 @@
+<h2 id="menu3"><a href="#">Fechar</a></h2>
+<h2 id="menu2"><a href="#">Menu</a></h2>
+<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Sidebar-1') ) : ?><?php endif; ?>
